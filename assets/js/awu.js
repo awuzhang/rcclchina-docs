c940fe6a4e772632b373431e157bb01a60f46a2e
awuzhang <awuzhang@gmail.com>
2016-01-06 17:14:16 +0800

    checkin 两个图标

--- assets/js/awu.js
+++ assets/js/awu.js
@@ -4,11 +4,11 @@ $(function () {
 
   $(document).on("pageInit", function(e) {
     if($.device.isWeixin) {
-      $('#myhead').css({'display': 'none'});
     }
   })
 
   $(document).on("pageInit", "#page-index", function(e) {
+    /* 测试用 */
     $(".label").on('touchstart', function(){
       var $span = $(this).find('span'), $inp = $(this).find('input');
       $span.addClass('animated s50p').on('webkitTransitionEnd transitionend', function(){
@@ -17,8 +17,40 @@ $(function () {
     $(".label2").on('touchstart', function(){
       $(this).addClass('label3');
     });
+    $("#picker").picker({
+        toolbarTemplate: '<header class="bar bar-nav">\
+        <button class="button button-link pull-right close-picker">确定</button>\
+        <h1 class="title">标题</h1>\
+        </header>',
+        cols: [
+          {
+            textAlign: 'center',
+            values: ['1','2','3','4']
+          }
+        ]
+      });
   });
   
+  $(document).on("pageInit", "#page-route", function(e) {
+    $(".popup-ships .content").scroller({
+    	type: 'js'
+    });
+
+	$('.popup-ships .label-checkbox').on('click', function(){
+		$('.popup-ships .label-checkbox.label-checked').removeClass('label-checked');
+		$(this).addClass('label-checked');
+		$.closeModal('.popup-ships'); 
+	});
+	$('.popup-portSelect .label-checkbox').on('click', function(){
+		$(this).addClass('label-checked');
+		$.closeModal('.popup-portSelect');  
+	});
+	$('.popup-cportSelect .label-checkbox').on('click', function(){
+		$(this).addClass('label-checked');
+		$.closeModal('.popup-cportSelect');  
+	});
+  });
+
   $(document).on("pageInit", "#page-route-detail", function(e) {
     $(".swiper-container").swiper();
   });
